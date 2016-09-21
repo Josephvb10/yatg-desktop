a2b347a1fe005e8c80ffb836c1def6eaafd2e08f
Josephvb10 <joseph@cpokemon.com>
2016-09-21 00:14:38 -0600

    Primer commit

---
+++ src/cr/tec/yatg/desktop/gui/Main.java
@@ -0,0 +1,10 @@
+package cr.tec.yatg.desktop.gui;
+
+/**
+ * Created by joseph on 21/09/16.
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+}
