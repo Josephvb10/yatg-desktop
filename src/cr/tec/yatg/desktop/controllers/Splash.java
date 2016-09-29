55b85d560d480c20e889fe14c9af3bf004c0f875
Daniel Castro <DanielC@MacBook-Pro-de-Daniel.local>
2016-09-29 17:30:01 -0600

    Splash screen

---
+++ src/cr/tec/yatg/desktop/controllers/Splash.java
@@ -0,0 +1,14 @@
+package cr.tec.yatg.desktop.controllers;
+
+import javafx.fxml.FXML;
+/**
+ * Main GUI controller
+ * Created by daniel on 29/09/16.
+ */
+public class Splash {
+
+	@FXML 
+	private void pressPlay() {
+		
+	}
+}
