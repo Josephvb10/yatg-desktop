1c046adaff7454dbb1ce359c6cfb65cbf53dcd56
josephvb10 <joseph@cpokemon.com>
2016-09-29 17:07:44 -0600

    Implementado ActionMessage y paquete comms

---
+++ src/cr/tec/yatg/desktop/services/comms/Action.java
@@ -0,0 +1,8 @@
+package cr.tec.yatg.desktop.services.comms;
+
+/**
+ * Created by joseph on 29/09/16.
+ */
+public enum Action {
+	up, down, left, right, item, itemNext
+}
---
+++ src/cr/tec/yatg/desktop/services/comms/Player.java
@@ -0,0 +1,8 @@
+package cr.tec.yatg.desktop.services.comms;
+
+/**
+ * Created by joseph on 29/09/16.
+ */
+public enum Player {
+	p1, p2, p3, p4, bot
+}
