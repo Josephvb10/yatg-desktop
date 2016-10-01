e2a7535246d8d20f45416cff077a8b6cf58d34c5
Daniel Castro <DanielC@MacBook-Pro-de-Daniel.local>
2016-10-01 13:44:55 -0600

    interfaz de login

--- src/cr/tec/yatg/desktop/controllers/Login.java
+++ src/cr/tec/yatg/desktop/controllers/Login.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
@@ -29,28 +31,44 @@ public class Login {
 	@FXML
 	private TextField getPort;
 	
+	String playerUsername;
+	
+	String playerIp;
+	
+	String checkPort;
+
+	int playerPort;
 	@FXML 
-	private void pressLogin() {
-		
+	private void pressLogin(){
 		try {
+			checkPort = getPort.getText();
+            playerUsername=getUsername.getText();
+			playerIp=getIp.getText();
+			if (checkPort != "" & playerUsername !="" & playerIp !=""){
+				playerPort = Integer.parseInt(checkPort);
+				if (0 <= playerPort & playerPort <= 9999){
+					System.out.println("Entered Username: "+getUsername.getText());
+					FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../resources/views/main.fxml"));
+					Parent mainScreen = fxmlLoader.load();
+					Stage stage = new Stage();
+					stage.initModality(Modality.APPLICATION_MODAL);
+					stage.setTitle("Yet Another Tron Game");
+					stage.setResizable(false);
+					stage.setScene(new Scene(mainScreen));
+		
+					Stage mainStage = (Stage) loginButton.getScene().getWindow();
+					mainStage.close();
+		
+					stage.setOnCloseRequest(e -> {
+						Platform.exit();
+						System.exit(0);
+					});
+					stage.show();
 			
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../resources/views/main.fxml"));
-			Parent mainScreen = fxmlLoader.load();
-			Stage stage = new Stage();
-			stage.initModality(Modality.APPLICATION_MODAL);
-			stage.setTitle("Yet Another Tron Game");
-			stage.setResizable(false);
-			stage.setScene(new Scene(mainScreen));
-
-			Stage mainStage = (Stage) loginButton.getScene().getWindow();
-			mainStage.close();
-
-			stage.setOnCloseRequest(e -> {
-				Platform.exit();
-				System.exit(0);
-			});
-			stage.show();
-		} catch (Exception e) {
+	
+			
+				}
+				} }catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
