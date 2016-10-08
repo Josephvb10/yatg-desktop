bd170b58854f4204fb64fc4ff7e09e5f1f09aa34
josephvb10 <joseph@cpokemon.com>
2016-10-08 02:08:41 -0600

    Nueva versión de Output Message

--- src/cr/tec/yatg/desktop/services/comms/Action.java
+++
@@ -1,8 +0,0 @@
-package cr.tec.yatg.desktop.services.comms;
-
-/**
- * Created by joseph on 29/09/16.
- */
-public enum Action {
-	up, down, left, right, item, itemNext
-}
--- src/cr/tec/yatg/desktop/services/comms/OutputMessage.java
+++ src/cr/tec/yatg/desktop/services/comms/OutputMessage.java
@@ -13,26 +13,25 @@ public class OutputMessage {
 	 */
 	private int id;
 	private SimplePlayer player;
-	private ArrayList<Item> itemList;
-
+	private ArrayList<Item> itemList, powerupsList;
 
 	public OutputMessage() {
 		super();
 	}
 
-
-	public OutputMessage(SimplePlayer player, ArrayList<Item> itemList) {
+	public OutputMessage(SimplePlayer player, ArrayList<Item> itemList, ArrayList<Item> powerupsList) {
 		super();
 		this.player = player;
 		this.itemList = itemList;
+		this.powerupsList = powerupsList;
 	}
 
-
-	public OutputMessage(Troncycle player, GenericLinkedList<Item> itemList) {
+	public OutputMessage(Troncycle player, GenericLinkedList<Item> itemList, GenericLinkedList<Item> powerupsList) {
 		super();
 		id = getNextid();
 		importPlayer(player);
 		importItemList(itemList);
+		importPowerupsList(powerupsList);
 	}
 
 	private static int getNextid() {
@@ -59,13 +58,23 @@ public class OutputMessage {
 		this.player = player;
 	}
 
+
+	public ArrayList<Item> getPowerupsList() {
+		return powerupsList;
+	}
+
+	public void setPowerupsList(ArrayList<Item> powerupsList) {
+		this.powerupsList = powerupsList;
+	}
+
 	public void importPlayer(Troncycle player) {
-		SimplePlayer simplePlayer = new SimplePlayer(player.getOwner(), player.getSpeed(), player.getFuel(), player.getCurrentDirection(), player.getExtraTrail(), player.getPowerUpSteps(), player.getIsDead(), player.getPowerUpActivated());
+		SimplePlayer simplePlayer = new SimplePlayer(player.getOwner(), player.getSpeed(), player.getFuel(),
+				player.getCurrentDirection(), player.getExtraTrail(), player.getPowerUpSteps(), player.getIsDead(),
+				player.getPowerUpActivated());
 		importItemsPriorityQueue(player.getItemsQueue());
 		setPlayer(simplePlayer);
 
 	}
-
 
 	public ArrayList<Item> getItemList() {
 		return itemList;
@@ -88,6 +97,16 @@ public class OutputMessage {
 		this.itemList = newItemList;
 	}
 
+	public void importPowerupsList(GenericLinkedList<Item> powerupsList) {
+		GenericNode<Item> current = powerupsList.getHead();
+		ArrayList<Item> newPowerupsList = new ArrayList<>();
+		while (current != null) {
+			newPowerupsList.add(current.getData());
+			current = current.getNext();
+		}
+		this.powerupsList = newPowerupsList;
+	}
+
 	public void importItemsPriorityQueue(ItemsPriorityQueue itemsPriorityQueue) {
 		GenericNode<Item> current = itemsPriorityQueue.getHead();
 		ArrayList<Item> newItemsPriorityQueue = new ArrayList<>();
@@ -105,8 +124,4 @@ public class OutputMessage {
 		return messageJson;
 	}
 
-
 }
-
-
-
